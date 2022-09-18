672775b0733a0cd8c6418166358f98a26986c4af
Patryk-Krawiec <kontakt@krawiecpatryk.pl>
2022-09-18 21:18:37 +0200

    saving data into db - postmapping

--- src/main/java/com/krawiecpatryk/studentsystem/service/StudentService.java
+++ src/main/java/com/krawiecpatryk/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.krawiecpatryk.studentsystem.service;
 
+import com.krawiecpatryk.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
