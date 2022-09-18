f14e2b86b97aa9493e6a43668fbfe0ba0eba15e9
Patryk-Krawiec <kontakt@krawiecpatryk.pl>
2022-09-18 20:52:30 +0200

    database initial setup

--- src/main/java/com/krawiecpatryk/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/krawiecpatryk/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.krawiecpatryk.studentsystem.repository;
 
-public interface StudentRepository {
+import com.krawiecpatryk.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
 }
