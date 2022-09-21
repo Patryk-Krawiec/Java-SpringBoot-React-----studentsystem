10ec420fab31a8125736934077291b20b3c97f71
Patryk-Krawiec <kontakt@krawiecpatryk.pl>
2022-09-21 09:59:43 +0200

    getting students data out of database - getmapping

--- src/main/java/com/krawiecpatryk/studentsystem/controller/StudentController.java
+++ src/main/java/com/krawiecpatryk/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.krawiecpatryk.studentsystem.controller;
 import com.krawiecpatryk.studentsystem.model.Student;
 import com.krawiecpatryk.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -18,5 +17,9 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "Dodano nowego studenta";
+    }
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/krawiecpatryk/studentsystem/service/StudentService.java
+++ src/main/java/com/krawiecpatryk/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.krawiecpatryk.studentsystem.service;
 
 import com.krawiecpatryk.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
